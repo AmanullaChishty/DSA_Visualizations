44698f17e5ea0e402bf50c50772180f8e737d7e4
Mohiuddin Amanulla Chishty <44812512+AmanullaChishty@users.noreply.github.com>
2026-06-10 15:54:35 +0000

    Basics

---
+++ Linkedlist.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None #stores refernce to next node
+
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+
+n1.next = n2
+n2.next = n3
+
+print(n1.data)
+print(n1.next.data)
+print(n1.next.next.data)
