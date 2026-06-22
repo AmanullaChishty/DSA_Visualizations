ee117676a136f28589a04f2597b85f0bf5c2dbf8
Mohiuddin Amanulla Chishty <44812512+AmanullaChishty@users.noreply.github.com>
2026-06-22 19:39:30 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (65.30%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next #Saving 
+            curr.next = prev #Reversing
+
+            prev = curr #Advancing
+            curr = next_node #Advancing
+        
+        return prev
+
+        
