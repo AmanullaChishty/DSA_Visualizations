c0b6374e8ceaec050ec81f301bf154997538bffe
Mohiuddin Amanulla Chishty <44812512+AmanullaChishty@users.noreply.github.com>
2026-06-25 21:43:32 +0530

    Time: 1 ms (63.62%) | Memory: 27.7 MB (79.81%) - LeetSync

---
+++ 143-reorder-list/reorder-list.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+#Psuedo Code:
+# n should go to 2 position
+# n-1 should go to 
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+      
+        second = slow.next
+        slow.next = None
+
+        prev = None
+        curr = second
+
+        while curr:
+            next_node = curr.next
+            curr.next= prev
+
+            prev = curr
+            curr = next_node
+        
+        first = head
+        second = prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+
+            first.next = second
+            second.next = tmp1
+
+            first = tmp1
+            second = tmp2
+
+
+
+            
+
+
+        
