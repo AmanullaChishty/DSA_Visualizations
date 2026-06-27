2cda9f2846c88aff182d18ff77cb6fd13e4be674
Mohiuddin Amanulla Chishty <44812512+AmanullaChishty@users.noreply.github.com>
2026-06-27 11:51:26 +0530

    Time: 111 ms (65.26%) | Memory: 37.8 MB (98.09%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -9,33 +9,44 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        cur1 = headA
-        cur2 = headB
-        lenA=0
-        lenB=0
-
-        while cur1:
-            lenA+=1
-            cur1 = cur1.next
-        while cur2:
-            lenB+=1
-            cur2 = cur2.next
-
+        #Method-2
         cur1 = headA
         cur2 = headB
 
-        if lenA>lenB:
-            for _ in range(lenA-lenB):
-                cur1 = cur1.next
-        elif lenB > lenA:
-            for _ in range(lenB-lenA):
-                cur2 = cur2.next
+        while cur1!=cur2:
+            cur1 = cur1.next if cur1 else headB
+            cur2 = cur2.next if cur2 else headA
+
+        return cur1
+
+        # Method-1
+        # cur1 = headA
+        # cur2 = headB
+        # lenA=0
+        # lenB=0
+
+        # while cur1:
+        #     lenA+=1
+        #     cur1 = cur1.next
+        # while cur2:
+        #     lenB+=1
+        #     cur2 = cur2.next
+
+        # cur1 = headA
+        # cur2 = headB
+
+        # if lenA>lenB:
+        #     for _ in range(lenA-lenB):
+        #         cur1 = cur1.next
+        # elif lenB > lenA:
+        #     for _ in range(lenB-lenA):
+        #         cur2 = cur2.next
         
         
-        while cur1 and cur2:
-            if cur1 == cur2:
-                return cur1
-            cur1 = cur1.next
-            cur2 = cur2.next
-        return None
+        # while cur1 and cur2:
+        #     if cur1 == cur2:
+        #         return cur1
+        #     cur1 = cur1.next
+        #     cur2 = cur2.next
+        # return None
         
