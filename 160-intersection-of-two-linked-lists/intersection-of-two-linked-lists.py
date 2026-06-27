69c7d5f7dd6fab74d9574483fb9866d354503c78
Mohiuddin Amanulla Chishty <44812512+AmanullaChishty@users.noreply.github.com>
2026-06-27 10:26:29 +0530

    Time: 114 ms (50.43%) | Memory: 37.8 MB (98.09%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#Psuedo Code:
+# traverse both 
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        cur1 = headA
+        cur2 = headB
+        lenA=0
+        lenB=0
+
+        while cur1:
+            lenA+=1
+            cur1 = cur1.next
+        while cur2:
+            lenB+=1
+            cur2 = cur2.next
+        print(lenA,lenB)
+        cur1 = headA
+        cur2 = headB
+
+        if lenA>lenB:
+            for _ in range(lenA-lenB):
+                cur1 = cur1.next
+        else:
+            for _ in range(lenB-lenA):
+                cur2 = cur2.next
+        
+        
+        while cur1 and cur2:
+            if cur1 == cur2:
+                return cur1
+            cur1 = cur1.next
+            cur2 = cur2.next
+        return None
+        
