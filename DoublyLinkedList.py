234e24c59905be3e392b7d8b32158fe9d51d3f45
Mohiuddin Amanulla Chishty <44812512+AmanullaChishty@users.noreply.github.com>
2026-07-17 15:36:01 +0530

    Implement prepend method in DoublyLinkedList
    Add prepend method to insert a node at the beginning.

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -21,7 +21,17 @@ class DoublyLinkedList:
             self.tail.next = new_node
             new_node.prev = self.tail
             self.tail = new_node
-            
+        self.size+=1
+        
+    def prepend(self, val):
+        new_node = Node(val)
+        if self.size == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
         self.size+=1
     
     
